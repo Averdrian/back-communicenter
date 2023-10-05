c21fbae2290f505f195f5907de345b0fbeb9ef33
Adrian <adrianestevezg@gmail.com>
2023-10-05 14:52:08 +0200

    cors policy and simple get call with json response

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from config import Config
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
+CORS(app, origins=os.getenv("FRONT_URL"))
 
 
 app.config.from_object(Config)
@@ -10,6 +13,11 @@ app.config.from_object(Config)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = jsonify({'nombre': 'John', 'edad': 30, 'ciudad': 'Nueva York'})
+    response = make_response(data, 202)
+    return response
 
 if __name__ == '__main__':
     app.run(port=8000)
