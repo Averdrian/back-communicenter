52ef47b3369c815b4d3346467c07eba259a11747
Adrian <adrianestevezg@gmail.com>
2023-10-06 16:34:36 +0200

    restructure a little and order into app.py

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, jsonify, make_response
-from config import Config
-from flask_cors import CORS
 import os
 
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-
+#Create the flask instance
 app = Flask(__name__)
+
+#Add the URL from the frontend to the cors policy
+from flask_cors import CORS
 CORS(app, origins=os.getenv("FRONT_URL"))
 
-
+#Gets our Config object into the instance configuration
+from config import Config
 app.config.from_object(Config)
 
+#Integrate SQLAlchemy to database and FlaskMigrate to migrations
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 db = SQLAlchemy(app)
-from src.models.User import User
-
+from models.user import User #Import models to migrate them
 migrate = Migrate(app, db, directory='src/migrations')
 
+
+#Temporary routes, deleting soon
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -28,5 +31,7 @@ def get_data():
     response = make_response(data, 202)
     return response
 
+
+#Run the application
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run()
