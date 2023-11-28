80708e1fd25ef5aac2c44453132b9c1febdb95ba
Averdrian <adrianestevezg@gmail.com>
2023-11-28 21:14:14 +0100

    moved imports in app

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 from flask import Flask
 import os
-from settings import Settings
+from settings import Settings, login_manager
 from database import db
+from config import configure_login_manager, configure_database, register_blueprints
+from flask_cors import CORS
+
 
 #Create the flask instance
 app = Flask(__name__)
 
 #Add the URL from the frontend to the cors policy
-from flask_cors import CORS
 CORS(app, origins=os.getenv("FRONT_URL"))
 
 #Gets our Config object into the instance configuration
 app.config.from_object(Settings)
 
 #Authentication, login manager
-from settings import login_manager
-from config import configure_login_manager
 configure_login_manager(login_manager, app)
 
 with app.app_context():
-    from config import register_blueprints, configure_database
     register_blueprints(app)
     configure_database(app, db)
 
