d31ee25aacbe16131207767ccf95451fce25b7e2
Averdrian <adrianestevezg@gmail.com>
2024-04-15 12:38:06 +0200

    authorization decorators

---
+++ src/utils/authentication.py
@@ -0,0 +1,24 @@
+from flask_login import current_user
+from flask import jsonify
+from src.models import UserRole
+
+
+def chief_required(func):
+    @login_required
+    def decorate_func(*args, **kwargs):
+        if not _is_chief(current_user):
+            return jsonify({'error': "User must be chief"})
+        else: return func(*args, **kwargs)
+    return decorate_func 
+
+def login_required(func):
+    def decorate_func(*args, **kwargs):
+        if not current_user.is_authenticated:
+            return jsonify({'error': 'Unauthorized'}), 401
+        return func(*args, **kwargs)
+    return decorate_func
+
+
+def _is_chief(user):
+    return user.role == UserRole.CHIEF.value
+   
